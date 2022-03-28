2a16c52df1d24b68c71ea9a8972d45b61c5efa9e
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-03-28 13:55:57 -0500

    Prompted a user to enter an integer and store that value in an int variable using the nextInt method.

--- src/ConsoleExercise.java
+++ src/ConsoleExercise.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
+
+
 public class ConsoleExercise {
     public static void main(String[] args){
 
         double pi = 3.14159;
-        System.out.format("The value of pi is approvimately %.2f.", pi);
+        System.out.format("The value of pi is approvimately %.2f. \n", pi);
+
+
+        Scanner num = new Scanner(System.in);
+
+        System.out.print("Enter something: \n");    // Gets ask to input something
+        Integer userInput = num.nextInt();         // takes in an input and takes Integer
+        System.out.println("You entered: --> \"" + userInput + "\" <-- \n"); // output the variable
+
     }
 }

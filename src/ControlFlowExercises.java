daeb63ed165f9b870b541ce029edbc4bc140af47
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-03-28 16:04:36 -0500

    Created a while loop that will run as long as 15 and incremented by 1.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,8 +3,20 @@ public class ControlFlowExercises {
 
 
 
+//---- Loops Basics
+// --  While
+// [X] Create an integer variable i with a value of 5.
+// [X] Create awhile loop that runs so long as i is less than or equal to 15
+// [X] Each loop iteration, output the current value of i, then increment i by one.
+// [X] Your output should look like this:
+//  5 6 7 8 9 10 11 12 13 14 15
 
 
+        int i = 5;
+        while (i <= 15) {
+            System.out.println(i);
+            i++;
+        }
 
 
 

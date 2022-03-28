99631e01a17f1db29077b41ce5e71d34cca1ca98
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-03-28 14:09:33 -0500

    Prompt a user to enter 3 words, and store each of them in a separate variable.

--- src/ConsoleExercise.java
+++ src/ConsoleExercise.java
@@ -5,15 +5,33 @@ import java.util.Scanner;
 public class ConsoleExercise {
     public static void main(String[] args){
 
-        double pi = 3.14159;
-        System.out.format("The value of pi is approvimately %.2f. \n", pi);
+//        double pi = 3.14159;
+//        System.out.format("The value of pi is approvimately %.2f. \n", pi);
+
+//
+//        Scanner num = new Scanner(System.in);
+//
+//        System.out.print("Enter something: \n");    // Gets ask to input something
+//        Integer userInput = num.nextInt();         // takes in an input and takes Integer
+//        System.out.println("You entered: --> \"" + userInput + "\" <-- \n"); // output the variable
+//
+//
+
+        Scanner randomWord = new Scanner(System.in);
+
+        System.out.println("Enter three names: ");
+        // String input
+        String name = randomWord.nextLine();
+        String name2 = randomWord.nextLine();
+        String name3 = randomWord.nextLine();
+        // Output input by user
+        System.out.println("Person 1: " + name);
+        System.out.println("Person 2: " + name2);
+        System.out.println("Person 3: " + name3);
 
 
-        Scanner num = new Scanner(System.in);
 
-        System.out.print("Enter something: \n");    // Gets ask to input something
-        Integer userInput = num.nextInt();         // takes in an input and takes Integer
-        System.out.println("You entered: --> \"" + userInput + "\" <-- \n"); // output the variable
+
 
     }
 }

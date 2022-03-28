8b8b2a828afabb8b6196f4582a3fbc5cd4365644
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-03-28 13:36:04 -0500

    Created a variable with pi and created a System.out.format to display the pi with a string.

--- src/ConsoleExercise.java
+++ src/ConsoleExercise.java
@@ -1,8 +1,7 @@
 public class ConsoleExercise {
     public static void main(String[] args){
 
-
-
-
+        double pi = 3.14159;
+        System.out.format("The value of pi is approvimately %.2f.", pi);
     }
 }

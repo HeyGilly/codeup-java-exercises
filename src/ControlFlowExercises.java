3ba6a4a1f30acbf0ad5b656a7824c5ea21e4659b
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-03-29 11:58:40 -0500

    Created a program that will take your grade and output the results of your grade. A+, A-, etc.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -15,55 +15,71 @@ public class ControlFlowExercises {
         // [X] Your output should look like this:
         //  5 6 7 8 9 10 11 12 13 14 15
 
+        System.out.println("-----------------------------------------------------------------------------");
+        System.out.println("Exercise 1");
+        System.out.println("-----------------------------------------------------------------------------");
 
-//        int i = 5;
-//        while (i <= 15) {
-//            System.out.println(i);
-//            i++;
-//        }
+        int i = 5;
+        while (i <= 15) {
+            System.out.println(i);
+            i++;
+        }
 
+        System.out.println("-----------------------------------------------------------------------------");
+        System.out.println("Exercise 2");
+        System.out.println("-----------------------------------------------------------------------------");
         //----- Do While
         //--- Create a do-while loop that will count by 2's starting with 0 and ending at 100.
         // [X] Follow each number with a new line.
         // [X] Alter your loop to count backwards by 5's from 100 to -10. Create a do-while loop that starts at 2, and
         // [X] displays the number squared on each line while the number is less than 1,000,000. Output should equal:
-//        int i = 100;
-//        do {
-//            System.out.println(i);
-//            i-=5;
-//        } while (i > -15);
+        int iy = 100;
+        do {
+            System.out.println(iy);
+            iy-=5;
+        } while (iy > -15);
 
-        //----- For
-        // Refactor the previous two exercises to use a for loop instead.
 
-//            for (var x = 100; x>-15;x-=5){
-//                System.out.println(x);
-//            }
+        System.out.println("-----------------------------------------------------------------------------");
+        System.out.println("Exercise 2B");
+        System.out.println("-----------------------------------------------------------------------------");
+//        ----- For
+//         Refactor the previous two exercises to use a for loop instead.
 
+            for (var x = 100; x>-15;x-=5){
+                System.out.println(x);
+            }
+
+
+        System.out.println("-----------------------------------------------------------------------------");
+        System.out.println("Exercise 3");
+        System.out.println("-----------------------------------------------------------------------------");
         //----- FizzBuzz
         // [X] Write a program that prints the numbers from 1 to 100.
         // [X] For multiples of three: print “Fizz” instead of the number.
         // [X] For the multiples of five: print “Buzz”.
         // [X] For numbers which are multiples of both three and five: print “FizzBuzz”.
-//        int count = 1;
-//        while (count < 100) {
-//            if (count % 5 == 0 && count % 3 == 0){
-//                System.out.println("FizzBuzz");
-//                count++;
-//            }
-//            if (count % 3 == 0){
-//                System.out.println("Fizz");
-//                count++;
-//            }
-//            if (count % 5 == 0){
-//                System.out.println("Buzz");
-//                count++;
-//            }
-//            System.out.println(count);
-//            count++;
-//        }
+        int count = 1;
+        while (count < 100) {
+            if (count % 5 == 0 && count % 3 == 0){
+                System.out.println("FizzBuzz");
+                count++;
+            }
+            if (count % 3 == 0){
+                System.out.println("Fizz");
+                count++;
+            }
+            if (count % 5 == 0){
+                System.out.println("Buzz");
+                count++;
+            }
+            System.out.println(count);
+            count++;
+        }
 
-
+        System.out.println("-----------------------------------------------------------------------------");
+        System.out.println("Exercise 4");
+        System.out.println("-----------------------------------------------------------------------------");
         //----- Display a table of powers.
         //  [X] Prompt the user to enter an integer.
         //  [X] Display a table of squares and cubes from 1 to the value entered.
@@ -71,66 +87,79 @@ public class ControlFlowExercises {
         //  [ ] Assume that the user will enter valid data.
         //  [ ] Only continue if the user agrees to.
 
-//        System.out.println("-----------------------------------------------------------------------------");
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.print("What number would you like to go up to? ");
-//        Integer num = Integer.valueOf(scanner.next());
-//        int squareValue = num * num * num;
-//        int cubedvalue = num * num * num * num;
-//        if (num == 0) {
-//            System.out.println("GameOver!");
-//        } else if (num == 1){
-//            System.out.println("number   | squared  | cubed");
-//            System.out.println("-----------------------------");
-//            System.out.println(num + "        |   " + squareValue + "      |   " + cubedvalue);
-//            System.out.println("-----------------------------");
-//         } else {
-//            System.out.println("number   | squared  | cubed");
-//            System.out.println("-----------------------------");
-//            System.out.println(1 + "        |   " + 1 + "      |   " + 1);
-//            System.out.println("-----------------------------");
-//            System.out.print("Would you like to continue? yes / no \n");
-//            String continueValue = scanner.next();
-//            if (continueValue.equals("yes")){
-//                System.out.println("-----------------------------");
-//                int count = 1;
-//                do{
-//                    int squareCount = count * count * count;
-//                    int cubedCount = count * count * count * count;
-//                    System.out.println(count+"        |   "+ squareCount +"      |   "+ cubedCount);
-//                    System.out.println("-----------------------------");
-//                    count++;
-//                } while(count <= num );
-//            }else{
-//                System.out.println("GameOver!");
-//            }
-//        }
-
-
         Scanner scanner = new Scanner(System.in);
-        System.out.print("What was your Grade? ");
-        Integer num2 = Integer.valueOf(scanner.next());
+        System.out.println("              What number would you like to go up to? \n" +
+                "                   (Please insert a number)\n" +
+                "\n" +
+                "\n");
+        Integer num = Integer.valueOf(scanner.next());
+        int squareValue = num * num * num;
+        int cubedvalue = num * num * num * num;
+        if (num == 0) {
+            System.out.println("GameOver!");
+        } else if (num == 1){
+            System.out.println("number   | squared  | cubed");
+            System.out.println("-----------------------------");
+            System.out.println(num + "        |   " + squareValue + "      |   " + cubedvalue);
+            System.out.println("-----------------------------");
+         } else {
+            System.out.println("number   | squared  | cubed");
+            System.out.println("-----------------------------");
+            System.out.println(1 + "        |   " + 1 + "      |   " + 1);
+            System.out.println("-----------------------------");
+            System.out.print("Would you like to continue? yes / no \n");
+            String continueValue = scanner.next();
+            if (continueValue.equals("yes")){
+                System.out.println("-----------------------------");
+                int count2 = 1;
+                do{
+                    int squareCount = count2 * count2 * count2;
+                    int cubedCount = count2 * count2 * count2 * count2;
+                    System.out.println(count2+"        |   "+ squareCount +"      |   "+ cubedCount);
+                    System.out.println("-----------------------------");
+                    count2++;
+                } while(count2 <= num );
+            }else{
+                System.out.println("GameOver!");
+            }
+        }
+
+
+
+
+        System.out.println("-----------------------------------------------------------------------------");
+        System.out.println("Exercise 5");
+        System.out.println("-----------------------------------------------------------------------------");
+
+
+        //--- Convert given number grades into letter grades.
+
+                Scanner scanner2 = new Scanner(System.in);
+        System.out.println("              What is your grade? \n" +
+                "\n" +
+                "\n" +
+                "\n");        Integer num2 = Integer.valueOf(scanner2.next());
 
         System.out.println(num2);
         System.out.println("Would you like to continue? yes / no \n");
-        String continueValue2 = scanner.next();
+        String continueValue2 = scanner2.next();
 
         if (continueValue2.equals("yes")){
                 if(num2 > 94){
                     System.out.println("You made a A+");
-                } else if(num2 > 90){
+                } else if(num2 >= 90){
                     System.out.println("You made a A-");
-                } else if(num2 > 85){
+                } else if(num2 >= 85){
                     System.out.println("You made a B+");
-                } else if(num2 > 80){
+                } else if(num2 >= 80){
                     System.out.println("You made a B-");
-                } else if(num2 > 75){
+                } else if(num2 >= 75){
                     System.out.println("You made a C+");
-                } else if(num2 > 70){
+                } else if(num2 >= 70){
                     System.out.println("You made a C-");
-                } else if(num2 > 65){
+                } else if(num2 >= 65){
                     System.out.println("You made a D+");
-                } else if(num2 > 60){
+                } else if(num2 >= 60){
                     System.out.println("You made a D-");
                 } else if(num2 < 60){
                     System.out.println("You failed!");
@@ -138,14 +167,6 @@ public class ControlFlowExercises {
             }else{
                 System.out.println("We dont have to talk about your grade.");
             }
-
-
-
-
-
-
-
-
 
     }
 }

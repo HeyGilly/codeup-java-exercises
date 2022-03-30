743bfa935a3128156876d93fb9c686e419c3256e
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-03-30 10:50:20 -0500

    trying to create a do while loop that ask if you want ot ask a question again.

--- src/Bob.java
+++ src/Bob.java
@@ -1,28 +1,35 @@
 import java.util.Scanner;
 
 public class Bob {
+
     public static void main(String[] args) {
-
-
-        String[] bobAnswers = {"Sure", "Whoa, chill out!", "Fine. Be that way", "Whatever"};
-        int min = 0;
-        int max = 4;
-        int num = (int) (Math.random()*(max - min + 1)+min);
-        String bobSays = bobAnswers[num];
-
         Scanner scanner = new Scanner(System.in);
-        System.out.printf("Ask Bob a question, any question: \n");
-        String userInput = scanner.next();
-        System.out.printf(
-                        "Your question is: "+userInput +
-                        "\nBob's answer: "+ bobSays);
 
 
 
 
-
-
-
+            // Important Variables
+            String choiceContinue = "y";
+        do {
+            String[] bobAnswers = {"Sure", "Whoa, chill out!", "Fine. Be that way", "Whatever"};
+            int min = 0;
+            int max = 4;
+            int num = (int) (Math.random()*(max - min + 1)+min);
+            String bobSays = bobAnswers[num];
+            // Question
+            System.out.print("Ask Bob a question, any question: \n");
+            String userInput = scanner.next();
+            // Bob's Answer
+            System.out.print("Bob's answer: "+ bobSays);
+            System.out.println();
+            // Do you want to ask again?
+            System.out.print("\nAsk Again? (y/n): \n");
+            choiceContinue = scanner.next();
+            System.out.println();
+        }while(choiceContinue.equals("y"));
+        // When your done the scanner will close and GAME OVER
+        scanner.close();
+        System.out.println("Game-Over");
 
     }
 }

3be92ee0c26013d851d42b695c9ed6032f294d2e
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-03-31 11:32:35 -0500

    Initial Commit

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,24 @@
 public class Person {
+
+    private String name; // instance property
+
+
+
+    public String getName(){
+//TODO: return the person's name
+        return name;
+    }
+
+    public void setName(String name){
+//TODO: change the name field to the passed value
+        Person onePerson = new Person();
+        onePerson.name = "Bobby";
+    }
+    public void sayHello(){
+//TODO: print a message to the console using the person's name
+        System.out.println("Hey "+ this.name);
+    }
+
+
 }
+

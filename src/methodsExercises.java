0ddd35609275b9b9e13a7801ae846f898fc45eda
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-03-31 10:34:26 -0500

    Exercise 5 pushed, but still working on. When asked to play again it gets stuck and an error pops up.

--- src/methodsExercises.java
+++ src/methodsExercises.java
@@ -121,7 +121,10 @@ public class methodsExercises<min, max> {
 //        exercise3(exercise3Num);
 
         //Exercise #4
-        exercise4();
+//        exercise4();
+
+        //Exercise #5
+        exercise5();
 
     }
 
@@ -158,8 +161,61 @@ public class methodsExercises<min, max> {
 
 
     public static void exercise5(){
+        String choice = "y";
+        while (choice.equalsIgnoreCase("y")){
+            // Random
+            Random rand = new Random();
+            Scanner scanner = new Scanner(System.in);
+
+
+            //Start of exercise
+            System.out.println("------------------Exercise #5-------------------------");
+            System.out.println("Welcome to the EXCITING NEW number guessing Game!");
+            System.out.println("Rules: Guess the number between 1-100 and we will tell you HIGHER, LOWER, or if your " +
+                    "right on the " +
+                    "bullet.");
+            System.out.println("Good Luck!");
+            System.out.println();
+            int goldenNumber = rand.nextInt( 100);
+
+
+            formulaGuessingGame(goldenNumber);
+
+
+            //Want to roll again with new dice
+            System.out.println("Roll again? (y/n): ");
+            choice = scanner.nextLine();
+            System.out.println();
+            scanner.close();
+        }
 
     }
 
 
+    public static void formulaGuessingGame(int goldenNum){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(goldenNum);
+        System.out.println("What is the Golden Number? \n");
+        int userGuessedNumber  = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("the Golden Number is "+goldenNum);
+
+        if ( userGuessedNumber == goldenNum){
+            System.out.println("--------------------------- \n");
+            System.out.println("BING ");
+            System.out.println("BONG ");
+            System.out.println("WINNER WINNER ");
+            System.out.println("CHICKEN DINNER");
+            System.out.println();
+        } else if ( userGuessedNumber>goldenNum){
+            System.out.println("--------------------------- ");
+            System.out.println("LOWER ");
+
+            formulaGuessingGame(goldenNum);
+        } else {
+            System.out.println("---------------------------");
+            System.out.println("HIGHER");
+            formulaGuessingGame(goldenNum);
+        }
     }
+}

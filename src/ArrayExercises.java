af69fab7677d27c5934425185e79ff556b3e5023
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-04-04 12:10:36 -0500

    Created an array with 3 objects, working on creating a static method named addperson.

--- src/ArrayExercises.java
+++ src/ArrayExercises.java
@@ -1,10 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ArrayExercises {
 
 
     public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
+
+
+        String[] Person = {"John", "Paul", "Ringo"};
+        System.out.println("Array:\n"+ Arrays.toString(Person));
 
     }
 
 
+    public static void addPerson( int[] arrayList, String[] personToBeAdded) {
+        int n = arrayList.length;
 
+        int[] newArray = new int[n + 1];
+        System.out.println(Arrays.toString(newArray));
+
+
+
+        for (int people : newArray){
+            System.out.println(people);
+        }
+    }
 }
+

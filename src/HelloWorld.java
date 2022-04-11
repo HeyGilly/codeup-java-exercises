17a23633e0c31adcb285d5cc44efefa180febf76
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-04-10 22:50:24 -0500

    looked at the max value of an integer

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,7 @@
-import java.sql.SQLOutput;
 
 public class HelloWorld {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         System.out.println("Hello World!");
 
@@ -17,7 +16,7 @@ public class HelloWorld {
 
         // Change your code to assign a character value to myString.
 //         myString = 'H';
-         // This is not working because myString is a String and not a char data type.
+        // This is not working because myString is a String and not a char data type.
         // You will have to use value to convert different types of value into strings.
         // here we are using  char to string
         myString = String.valueOf('H');
@@ -42,6 +41,49 @@ public class HelloWorld {
         //which converts larger types into smaller types.
         // When using this casting you will lose data by the 7th decimal.
 
+        //Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
+        myNumber = 123L;
+        System.out.println(myNumber);
+        // Automatically cahnges the numbers to long 123.
+
+        //Change your code to assign the value 123 to myNumber.
+        myNumber = 123;
+        System.out.println(myNumber);
+
+        //Change your code to declare myNumber as a float. Assign the value 3.14 to it.
+        // What happens? What are two ways we could fix this?
+        myNumber = (long) 3.14f;
+        float vOut = myNumber;
+        System.out.println(vOut);
+
+
+        int x = 5;
+        System.out.println(x++);    // Shows five because the adding of one is after it shows
+        System.out.println(x);      // So when it gets here you are seeing the add so the variable becomes 6
+        System.out.println(++x);     // Here the adding is before the variable so it will change by the time it shows 7
+
+
+        // You are not able to make a variable named class due to being a keyword.
+        // Java is full of classes and classes are just template of objects.
+//
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//
+//        int x = 4;
+//        x += 5;
+//
+//        x = 3;
+//        int y = 4;
+//        y *= x;
+//
+//
+//         x = 10;
+//         y = 2;
+//        x /= y;
+//        y -= x;
+        
+        System.out.println(Integer.MAX_VALUE);
 
 
     }

64187e1989e41c23fb4ddab0309611bd2f06495c
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-04-10 21:39:17 -0500

    Working on syntax, types, and variables in java.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,18 @@
 public class HelloWorld {
 
     public static void main(String[] args){
+
         System.out.println("Hello World!");
+
+        //Exercise - Syntax, Types, and Variables
+
+        int myFavoriteNumber = 3;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "New world!";
+        System.out.println(myString);
+
+
+
     }
-
-
 }

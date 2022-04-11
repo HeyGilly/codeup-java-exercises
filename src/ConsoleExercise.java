16c9ed931ecc5ebe3e6c4356d8c277348f547e5b
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-04-10 23:06:57 -0500

    looked at the max value of an integer

--- src/ConsoleExercise.java
+++ src/ConsoleExercise.java
@@ -5,42 +5,46 @@ import java.util.Scanner;
 public class ConsoleExercise {
     public static void main(String[] args){
 
-//        double pi = 3.14159;
-//        System.out.format("The value of pi is approvimately %.2f. \n", pi);
+        System.out.println("---------Exercise #1-----------\n");
 
-//
-//        Scanner num = new Scanner(System.in);
-//
-//        System.out.print("Enter something: \n");    // Gets ask to input something
-//        Integer userInput = num.nextInt();         // takes in an input and takes Integer
-//        System.out.println("You entered: --> \"" + userInput + "\" <-- \n"); // output the variable
-//
-//
+        double pi = 3.14159;
+        System.out.format("The value of pi is approximately %.2f. \n", pi);
 
-//        Scanner randomWord = new Scanner(System.in);
-//
-//        System.out.println("Enter three names: ");
-//        // String input
-//        String name = randomWord.nextLine();
-//        String name2 = randomWord.nextLine();
-//        String name3 = randomWord.nextLine();
-//        // Output input by user
-//        System.out.println("Person 1: " + name);
-//        System.out.println("Person 2: " + name2);
-//        System.out.println("Person 3: " + name3);
-//
+        System.out.println("---------Exercise #2-----------\n");
+        Scanner num = new Scanner(System.in);
+        System.out.print("Enter an integer: \n");    // Gets ask to input something
+        int userInput = num.nextInt();         // takes in an input and takes Integer
+        System.out.println("You entered: --> \"" + userInput + "\" <-- \n"); // output the variable
 
-//        Scanner helloWorld = new Scanner(System.in);
-//        System.out.println("Enter a Sentence: ");
-//        String longSentence = helloWorld.nextLine();
-//        System.out.println(longSentence);
+        System.out.println("---------Exercise #3-----------\n");
+
+        System.out.println("Enter three names: ");
+        // String input
+        System.out.println("One: ");
+        String name1 = num.nextLine();
+        System.out.println("Two: ");
+        String name2 = num.nextLine();
+        System.out.println("Three: ");
+        String name3 = num.nextLine();
+        // Output input by user
+        System.out.println("Person 1: " + name1);
+        System.out.println("Person 2: " + name2);
+        System.out.println("Person 3: " + name3);
+
+        System.out.println("---------Exercise #4-----------\n");
+
+        System.out.println("Enter a Sentence: ");
+        String longSentence = num.nextLine();
+        System.out.println(longSentence);
+
+        System.out.println("---------Exercise #5-----------\n");
 
         Scanner classroom = new Scanner(System.in);
         System.out.println("Enter the width and height of the classroom");
-        //
-        Integer Width= classroom.nextInt();
-        Integer Height = classroom.nextInt();
-        //
+
+        int Width= classroom.nextInt();
+        int Height = classroom.nextInt();
+
         System.out.println("Area: " + (Width * Height));
         System.out.println("Perimeter:  " + ((2*Width)+(2*Height)));
 
